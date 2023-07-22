6875a4ace1ea9b85e2e8bb4ede4ebb60c091a5d2
CyberBrayan <brayan.calcina@hotmail.com>
2023-07-22 16:52:25 -0500

    Version PRD

---
+++ src/main/java/com/evaluation/user/openapi/OpenAPIConfiguration.java
@@ -0,0 +1,18 @@
+package com.evaluation.user.openapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class OpenAPIConfiguration {
+	
+	@Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"));
+    }
+
+}
